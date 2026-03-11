7e61537d29db7b7a5841a9b97b35484754035dbc
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-03-11 15:59:01 +0530

    rearranged all the patterns

--- Patterns/Medium_Pattern.java
+++ Patterns/Medium_Pattern.java
@@ -5,8 +5,9 @@ class Medium_Pattern{
         // pattern3(5);
         // pattern4(5);
         // pattern5(5);
-        // pattern6(5);
-        pattern7(5);
+        pattern6(5);
+        // pattern7(5);
+        pattern8(5);
     }
 
     public static void pattern1(int n){
@@ -70,6 +71,17 @@ class Medium_Pattern{
     }
 
     public static void pattern6(int n){
+        char ch = 'A';
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern7(int n){
         for(int i = 1 ; i <= n ; i++){
             char ch = 'A';
             for(int b = n - 1 ; b >= i ; b--)
@@ -87,7 +99,7 @@ class Medium_Pattern{
         }
     }
 
-    public static void pattern7(int n){
+    public static void pattern8(int n){
         for(int i = 1 ; i <= n ; i++){
             char ch = (char) ('A' + n - i);
             for(char j = ch ; j <= 'E' ; j++){
