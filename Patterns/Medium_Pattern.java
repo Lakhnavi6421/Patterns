8d32e1991cd43e31ae009e488a2361eb001224ef
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-03-11 15:27:08 +0530

    more medium level patterns

--- Patterns/Medium_Pattern.java
+++ Patterns/Medium_Pattern.java
@@ -1,7 +1,9 @@
 class Medium_Pattern{
     public static void main(String[] args) {
         // pattern1(5);
-        pattern2(5);
+        // pattern2(5);
+        // pattern3(5);
+        pattern4(5);
     }
 
     public static void pattern1(int n){
@@ -27,6 +29,28 @@ class Medium_Pattern{
                 System.out.print("_");
             for(int j = i ; j >=1 ; j--)
                 System.out.print(j);
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n){
+        int val = 1;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j =  1 ; j <= i ; j++){
+                System.out.print(val);
+                val++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern4(int n){
+        for(int i = 1 ; i <= n ; i++){
+            char ch = 'A';
+            for(int j = 1; j <= i ; j++){
+                System.out.print(ch);
+                ch++;
+            }
             System.out.println();
         }
     }
