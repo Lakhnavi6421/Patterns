88dc444d7b3612191a3794ab2b4bf856a4e4d9b3
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-03-17 22:53:42 +0530

    Better approach added to find largest and smallest element

--- Striver/Array/Easy/Largest_Smallest_Element.java
+++ Striver/Array/Easy/Largest_Smallest_Element.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Largest_Smallest_Element {
     public static void main(String[] args) {
-        int [] arr = {4, 6, 2, 9, 45, 32, 12};
+        int [] arr = {4, 6, 1, 9, 46, 32, 12};
         int n = arr.length;
         int maximum = largest(arr, n);
         int minimum = smallest(arr, n);
@@ -17,8 +17,16 @@ public class Largest_Smallest_Element {
         if(n == 1) return arr[0];
 
         // Brute force approach
-        Arrays.sort(arr);
-        return arr[n-1];
+//        Arrays.sort(arr);
+//        return arr[n-1];
+
+//        Better approach
+        int max = arr[0];
+        for(Integer i : arr){
+            if(i > max)
+                max = i;
+        }
+        return max;
     }
 
     public static int  smallest(int [] arr, int n){
@@ -26,7 +34,14 @@ public class Largest_Smallest_Element {
         if(n == 1) return arr[0];
 
         // Brute force (By sorting)
-        Arrays.sort(arr);
-        return arr[0];
+//        Arrays.sort(arr);
+//        return arr[0];
+
+//        Better approach(Using recursive)
+        int min = arr[0];
+        for(Integer i : arr){
+            min = Math.min(i, min);
+        }
+        return min;
     }
 }
