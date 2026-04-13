18dde2beec4003e0e72030e5e00b5749838e8301
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-13 14:06:17 +0530

    optimal approach using two pointer

--- Striver/Array/Easy/Move_Zeros_to_the_end.java
+++ Striver/Array/Easy/Move_Zeros_to_the_end.java
@@ -3,7 +3,7 @@ package Array.Easy;
 public class Move_Zeros_to_the_end
 {
     public static void main(String[] args) {
-        int[] arr = {0, 0, 0, 1, 0, 0, 3, 0, 12};
+        int[] arr = {0, 0, 0, 1, 0, 0, 3, 0, 12, 5, 0, 34};
         int[] result = moveZeros(arr);
         for(int i : result)
             System.out.print(i + " ");
@@ -12,23 +12,44 @@ public class Move_Zeros_to_the_end
 
     public static int[] moveZeros(int[] arr){
         // Brute force using temporary array
-        int[] temp = new int[arr.length];
+//        int[] temp = new int[arr.length];
+//
+//        // Pointer for temp
+//        int index = 0;
+//
+//        // Traverse input array
+//        for(int i = 0 ; i < arr.length ; i++){
+//            if(arr[i] != 0){
+//                temp[index] = arr[i];
+//                index++;
+//            }
+//        }
+//
+//        // Copy temp back to original array
+//        for(int i = 0 ; i < arr.length ; i++)
+//            arr[i] = temp[i];
+//
+//        return arr;
 
-        // Pointer for temp
-        int index = 0;
-
-        // Traverse input array
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] != 0){
-                temp[index] = arr[i];
-                index++;
+        // Optimal approach using two pointer
+        int j = -1;
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            if(arr[i] == 0){
+                j = i;
+                break;
             }
         }
+        if(j == -1) return arr;
 
-        // Copy temp back to original array
-        for(int i = 0 ; i < arr.length ; i++)
-            arr[i] = temp[i];
-
+        for(int i = j+1 ; i < arr.length ; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
         return arr;
     }
 }
