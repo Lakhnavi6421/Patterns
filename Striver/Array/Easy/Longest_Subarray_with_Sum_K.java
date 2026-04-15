4972d16f866961ae3d1ca464a7c8dbe2fdc9acb2
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-15 23:34:46 +0530

    brute force and better for longest subarray of +ve and -ve

---
+++ Striver/Array/Easy/Longest_Subarray_with_Sum_K.java
@@ -0,0 +1,40 @@
+package Array.Easy;
+
+public class Longest_Subarray_with_Sum_K {
+    public static void main(String[] args) {
+        int arr[] = { -1, 1, 2, 5};
+        int k = 2;
+        System.out.println("The length of the longest subarray is: " + longestSubarray(arr, k));
+    }
+
+    public static int longestSubarray(int [] arr, int k){
+        // Brute force
+
+//        int n = arr.length;
+//        int len = 0;
+//        for(int i = 0; i < n; i++){
+//            for(int j = i ; j < n; j++){
+//                int sum = 0;
+//                for(int idx = i ; idx <= j ; idx++)
+//                    sum += arr[idx];
+//
+//                if(sum == k)
+//                    len = Math.max(len, j - i + 1);
+//            }
+//        }
+//        return len;
+
+        // Better approach using Two Loop
+        int n = arr.length;
+        int maxLength = 0;
+        for(int i = 0 ; i < n ; i++){
+            int sum = 0;
+            for(int j = i ; j < n ; j++){
+                sum += arr[j];
+                if(sum == k)
+                    maxLength = Math.max(maxLength, j - i + 1);
+            }
+        }
+        return maxLength;
+    }
+}
