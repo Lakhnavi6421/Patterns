7849cde471163635d16fcb34b39058535c085d0a
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-15 19:54:50 +0530

    optiaml sol 2 using xor op

--- Striver/Array/Easy/Find_missing_number.java
+++ Striver/Array/Easy/Find_missing_number.java
@@ -27,14 +27,29 @@ public class Find_missing_number {
 
 //        Optimal approach
         // Calculate sum of first N natural numbers
+//        int n = arr.length + 1;
+//        int sum = (n * (n + 1))/2;
+//
+//        int actualSum = 0;
+//        for(int i = 0 ;i < n - 1; i++)
+//            actualSum += arr[i];
+//
+//        // difference is the missing number
+//        return sum - actualSum;
+
+        // Optimal approach 2 using XOR operator
+
         int n = arr.length + 1;
-        int sum = (n * (n + 1))/2;
+        int xor1 = 0, xor2 = 0;
+        // xor all the elements & numbers from 1 to n-1
+        for(int i = 0 ; i < n - 1 ; i++){
+            xor2 ^= arr[i];    // xor of array elements
+            xor1 ^= (i+1);     // xor of numbers from 1 to n-1
+        }
 
-        int actualSum = 0;
-        for(int i = 0 ;i < n - 1; i++)
-            actualSum += arr[i];
+        xor1 ^= n;      // include n in the xor
+        // xor of xor1 and xor2 gives the missing number
 
-        // difference is the missing number
-        return sum - actualSum;
+        return xor1 ^ xor2;
     }
 }
