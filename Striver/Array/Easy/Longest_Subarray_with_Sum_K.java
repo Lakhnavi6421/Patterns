3c60dfae8c60668fc0f11084b87022ce0ce8473d
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-15 23:40:23 +0530

    optimal sol for longest subarray of +ve and -ve

--- Striver/Array/Easy/Longest_Subarray_with_Sum_K.java
+++ Striver/Array/Easy/Longest_Subarray_with_Sum_K.java
@@ -1,8 +1,11 @@
 package Array.Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Longest_Subarray_with_Sum_K {
     public static void main(String[] args) {
-        int arr[] = { -1, 1, 2, 5};
+        int arr[] = { -1, 1, 2, 0, 5};
         int k = 2;
         System.out.println("The length of the longest subarray is: " + longestSubarray(arr, k));
     }
@@ -25,16 +28,43 @@ public class Longest_Subarray_with_Sum_K {
 //        return len;
 
         // Better approach using Two Loop
+//        int n = arr.length;
+//        int maxLength = 0;
+//        for(int i = 0 ; i < n ; i++){
+//            int sum = 0;
+//            for(int j = i ; j < n ; j++){
+//                sum += arr[j];
+//                if(sum == k)
+//                    maxLength = Math.max(maxLength, j - i + 1);
+//            }
+//        }
+//        return maxLength;
+
+        // Optimal approach using *** HASHING ***
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLenght = 0;
         int n = arr.length;
-        int maxLength = 0;
+
         for(int i = 0 ; i < n ; i++){
-            int sum = 0;
-            for(int j = i ; j < n ; j++){
-                sum += arr[j];
-                if(sum == k)
-                    maxLength = Math.max(maxLength, j - i + 1);
+            sum += arr[i];
+
+            // Case 1: if full subarray from 0 to i  has sum = k
+            if(sum == k)
+                maxLenght = i + 1;
+
+            // Case 2: if (sum - k) was seen before
+            int rem = sum - k;
+            if(preSumMap.containsKey(rem)){
+                int len = i - preSumMap.get(rem);
+                maxLenght = Math.max(maxLenght, len);
             }
+
+            // Store the first occurence of a prefix sum
+            if(!preSumMap.containsKey(sum))
+                preSumMap.put(sum, i);
         }
-        return maxLength;
+        return maxLenght;
     }
 }
