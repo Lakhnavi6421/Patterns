0132f793e36e7805cd2c8c4fd66e611d576233ca
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-15 19:29:30 +0530

    optimal sol using n natural no.

--- Striver/Array/Easy/Find_missing_number.java
+++ Striver/Array/Easy/Find_missing_number.java
@@ -7,20 +7,34 @@ public class Find_missing_number {
     }
 
     public static int missingNum(int[] arr){
-        int n = arr.length + 1;
-        for(int i = 1 ; i <= n ; i++){
-            boolean found = false;
+        // Brute force
 
-            // check if 'i' exists in array using Linear search
-            for(int j = 0 ; j < n - 1; j++){
-                if(arr[j] == i){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found)
-                return i;
-        }
-        return -1;
+//        int n = arr.length + 1;
+//        for(int i = 1 ; i <= n ; i++){
+//            boolean found = false;
+//
+//            // check if 'i' exists in array using Linear search
+//            for(int j = 0 ; j < n - 1; j++){
+//                if(arr[j] == i){
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if(!found)
+//                return i;
+//        }
+//        return -1;
+
+//        Optimal approach
+        // Calculate sum of first N natural numbers
+        int n = arr.length + 1;
+        int sum = (n * (n + 1))/2;
+
+        int actualSum = 0;
+        for(int i = 0 ;i < n - 1; i++)
+            actualSum += arr[i];
+
+        // difference is the missing number
+        return sum - actualSum;
     }
 }
