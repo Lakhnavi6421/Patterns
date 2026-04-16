c47fae55a52758da14f67a446142a5fc7cb254d8
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-16 18:23:46 +0530

    better sol using Hashmap

--- Striver/Array/Medium/Find_Majority_Element_occurs.java
+++ Striver/Array/Medium/Find_Majority_Element_occurs.java
@@ -1,5 +1,8 @@
 package Array.Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Find_Majority_Element_occurs {
     public static void main(String[] args) {
         int[] arr = {2, 2, 1, 1, 1, 2, 2};
@@ -7,17 +10,34 @@ public class Find_Majority_Element_occurs {
     }
 
     public static int majorityElement(int[] arr){
+        // Brute force approach
+//        int n = arr.length;
+//
+//        for(int i = 0 ; i < n ; i++){
+//            int count = 0;
+//
+//            for(int j = 0 ; j < n ; j++){
+//                if(arr[i] == arr[j])
+//                    count++;
+//            }
+//            if(count > (n/2))
+//                return arr[i];
+//        }
+//        return -1;
+
+        // Better approach using ** HASHMAP **
+
         int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0 ; i < n ; i++){
-            int count = 0;
+            int val = map.getOrDefault(arr[i], 0);
+            map.put(arr[i], val + 1);
+        }
 
-            for(int j = 0 ; j < n ; j++){
-                if(arr[i] == arr[j])
-                    count++;
-            }
-            if(count > (n/2))
-                return arr[i];
+        for(Map.Entry<Integer, Integer> it : map.entrySet()){
+            if(it.getValue() > (n/2))
+                return it.getKey();
         }
         return -1;
     }
