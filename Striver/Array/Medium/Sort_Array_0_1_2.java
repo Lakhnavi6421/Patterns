3890fb8d35044096755a017a8b51b2120a0f5f88
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-18 21:58:50 +0530

    optimal sol added

--- Striver/Array/Medium/Sort_Array_0_1_2.java
+++ Striver/Array/Medium/Sort_Array_0_1_2.java
@@ -43,23 +43,47 @@ public class Sort_Array_0_1_2 {
 
         // Better approach 2
 
-        int cnt0= 0, cnt1= 0, cnt2= 0;
-        for(int i : arr){
-            if(i == 0)
-                cnt0++;
-            else if (i == 1)
-                cnt1++;
-            else
-                cnt2++;
+//        int cnt0= 0, cnt1= 0, cnt2= 0;
+//        for(int i : arr){
+//            if(i == 0)
+//                cnt0++;
+//            else if (i == 1)
+//                cnt1++;
+//            else
+//                cnt2++;
+//        }
+//
+//        for(int i = 0 ; i < cnt0 ; i++)
+//            arr[i] = 0;
+//        for(int i = cnt0 ; i < cnt0 + cnt1 ; i++)
+//            arr[i] = 1;
+//        for(int i = cnt0 + cnt1 ; i < arr.length ; i++)
+//            arr[i] = 2;
+//
+//        return arr;
+
+        /////////////////////////////////////////////////
+        // Optimal approach
+
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high){
+            if(arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
         }
-
-        for(int i = 0 ; i < cnt0 ; i++)
-            arr[i] = 0;
-        for(int i = cnt0 ; i < cnt0 + cnt1 ; i++)
-            arr[i] = 1;
-        for(int i = cnt0 + cnt1 ; i < arr.length ; i++)
-            arr[i] = 2;
-
         return arr;
     }
 
