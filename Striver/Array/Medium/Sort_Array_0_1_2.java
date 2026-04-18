cd5003c69a1d87f64b0d8d1710c2627a4e54f986
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-18 21:54:31 +0530

    better sol added

--- Striver/Array/Medium/Sort_Array_0_1_2.java
+++ Striver/Array/Medium/Sort_Array_0_1_2.java
@@ -42,6 +42,26 @@ public class Sort_Array_0_1_2 {
 //        return arr;
 
         // Better approach 2
+
+        int cnt0= 0, cnt1= 0, cnt2= 0;
+        for(int i : arr){
+            if(i == 0)
+                cnt0++;
+            else if (i == 1)
+                cnt1++;
+            else
+                cnt2++;
+        }
+
+        for(int i = 0 ; i < cnt0 ; i++)
+            arr[i] = 0;
+        for(int i = cnt0 ; i < cnt0 + cnt1 ; i++)
+            arr[i] = 1;
+        for(int i = cnt0 + cnt1 ; i < arr.length ; i++)
+            arr[i] = 2;
+
+        return arr;
     }
+
 
 }
