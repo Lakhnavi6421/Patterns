c799e68660b3fd322bc4e30bd76f37b6229aa049
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-21 22:14:06 +0530

    recursive and in build methods

--- Striver/BinarySearch/BinarySearch.java
+++ Striver/BinarySearch/BinarySearch.java
@@ -1,34 +1,71 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        int []arr = {2, 3, 4, 10, 40, 47};
-        int n = arr.length;
-        int x = 10;
-        int res = binarySearch(arr, 0, n -1, x);
+//        int []arr = {2, 3, 4, 10, 40, 47};
+//        int n = arr.length;
+//        int x = 4;
+//        int res = binarySearch(arr, 0, n -1, x);
+
+//        System.out.println("Element to be searched is : " + x);
+//
+//        if(res == -1)
+//            System.out.println("Element is not present in the array");
+//        else
+//            System.out.println("Element is present at index : " + res);
+
+
+        // ***** IN BUILD METHODS *****
+
+        int [] arr = {10, 20, 15, 4, 18};
+
+        // sorting the array
+        Arrays.sort(arr);
+
+        int x = 15;
+        int res = Arrays.binarySearch(arr, x);
         System.out.println("Element to be searched is : " + x);
 
         if(res == -1)
             System.out.println("Element is not present in the array");
         else
             System.out.println("Element is present at index : " + res);
+        
     }
 
     public static int binarySearch(int [] arr, int left, int right, int x){
         // ****** ITERATIVE METHOD ********
 
-        while(left <= right){
-            int mid = (left + right) / 2;
+//        while(left <= right){
+//            int mid = (left + right) / 2;
+//
+////            index of element returned
+//            if(arr[mid] == x)
+//                return mid;
+//
+//            else if (arr[mid] > x)
+//                right = mid - 1;
+//
+//            else
+//                left = mid + 1;
+//        }
+//        return -1;
 
-//            index of element returned
+
+        // ***** RECURSIVE METHOD *****
+
+        if(right >= left){
+            int mid = left + (right - left) / 2;
+
             if(arr[mid] == x)
                 return mid;
 
-            else if (arr[mid] > x)
-                right = mid - 1;
+            if(arr[mid] > x)
+                return binarySearch(arr, left, mid - 1, x);
 
-            else
-                left = mid + 1;
+            return binarySearch(arr, mid + 1, right , x);
         }
         return -1;
     }
