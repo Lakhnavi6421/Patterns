10987d5706fd6e31d6e0148ebc89a21e989a09f8
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-22 23:32:37 +0530

    optimal sol for upper bound

--- Striver/BinarySearch/OneDimArrays/UpperBound.java
+++ Striver/BinarySearch/OneDimArrays/UpperBound.java
@@ -3,7 +3,7 @@ package BinarySearch.OneDimArrays;
 public class UpperBound {
     public static void main(String[] args) {
         int[] arr = {3, 5, 8, 9, 15, 19}; // Sorted array
-        int x = 9;
+        int x = 19;
 
         int ind = upperBound(arr, x); // Call function
 
@@ -11,10 +11,29 @@ public class UpperBound {
     }
 
     public static int upperBound(int[] arr, int target){
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] > target)
-                return i;
+
+        // Brute force solution
+
+//        for(int i = 0 ; i < arr.length ; i++){
+//            if(arr[i] > target)
+//                return i;
+//        }
+//        return arr.length;
+
+        // Optimal solution
+        int low = 0, high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        return arr.length;
+        return ans;
     }
 }
