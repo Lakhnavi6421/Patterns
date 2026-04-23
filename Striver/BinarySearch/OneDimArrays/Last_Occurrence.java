89fc65b43b9a9678eee2121ff0219b2f8aac4d33
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-23 23:51:15 +0530

    optimal sol of last Occurrence

--- Striver/BinarySearch/OneDimArrays/Last_Occurrence.java
+++ Striver/BinarySearch/OneDimArrays/Last_Occurrence.java
@@ -1,20 +1,43 @@
 package BinarySearch.OneDimArrays;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Last_Occurrence {
     public static void main(String[] args) {
-        int[] arr = {1, 4, 12, 12, 12, 18, 19};
-        int target = 12;
+        List<Integer> list = Arrays.asList(1, 4, 12, 12, 12, 18, 19);
+        int target = 19;
 
-        System.out.println("The index of " + target + " is " + lastOccurrence(arr, target));
+        System.out.println("The index of " + target + " is " + lastOccurrence(list, target));
     }
 
-    public static int lastOccurrence(int arr[], int target){
+    public static int lastOccurrence(List<Integer> list, int target){
         // Brute force
+//        int ans = -1;
+//        for(int i = list.size() - 1 ; i >= 0 ; i--){
+//            if(list.get(i) == target){
+//                ans = i;
+//                break;
+//            }
+//        }
+//        return ans;
+
+        // Optimal sol using binary search method
+
+        int low = 0, high = list.size() - 1;
         int ans = -1;
-        for(int i = arr.length - 1; i >= 0 ; i--){
-            if(arr[i] == target){
-                ans = i;
-                break;
+        while (low <= high){
+            int mid = low + ((high - low) / 2);
+
+            if(list.get(mid) == target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if (list.get(mid) > target) {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
         return ans;
