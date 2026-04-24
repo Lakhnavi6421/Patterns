09d4cabdf406c624de07ddd38ea774fb0440a020
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-04-24 14:56:39 +0530

    optimal sol for floor and ceil

--- Striver/BinarySearch/OneDimArrays/FloorCeilFinder.java
+++ Striver/BinarySearch/OneDimArrays/FloorCeilFinder.java
@@ -3,7 +3,7 @@ package BinarySearch.OneDimArrays;
 public class FloorCeilFinder {
     public static void main(String[] args) {
         int arr[] = {3, 4, 4, 7, 8, 10};
-        int target = 5;
+        int target = 9;
         System.out.println("Floor: " + findFloor(arr, target));
         System.out.println("Ceil: " + findCeil(arr, target));
     }
@@ -11,12 +11,27 @@ public class FloorCeilFinder {
     public static int findFloor(int arr[], int target){
 
         // Brute force solution
+//        int ans = Integer.MIN_VALUE;
+//        for(int i : arr){
+//            if(i <= target)
+//                ans = i;
+//            else
+//                break;
+//        }
+//        return ans;
+
+        // Optimal Solution
+
+        int low = 0, high = arr.length - 1;
         int ans = Integer.MIN_VALUE;
-        for(int i : arr){
-            if(i <= target)
-                ans = i;
-            else
-                break;
+        while (low <= high){
+            int mid = low + ((high - low) / 2);
+            if(arr[mid] <= target){
+                ans = arr[mid];
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
         return ans;
     }
@@ -24,11 +39,27 @@ public class FloorCeilFinder {
     public static int findCeil(int arr[], int target){
 
         // Brute force solution
+//        int ans = Integer.MAX_VALUE;
+//        for(int i : arr){
+//            if(i >= target){
+//                ans = i;
+//                break;
+//            }
+//        }
+//        return ans;
+
+
+        // Optimal Solution
+
+        int low = 0, high = arr.length - 1;
         int ans = Integer.MAX_VALUE;
-        for(int i : arr){
-            if(i >= target){
-                ans = i;
-                break;
+        while (low <= high){
+            int mid = low + ((high - low) / 2);
+            if(arr[mid] >= target){
+                ans = arr[mid];
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
         return ans;
