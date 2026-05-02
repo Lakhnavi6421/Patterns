2eacfb27cfc28decb11d5e74558d6a6891208e5a
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-05-02 17:27:00 +0530

    optimal sol

--- Striver/BinarySearch/OneDimArrays/PeakElement.java
+++ Striver/BinarySearch/OneDimArrays/PeakElement.java
@@ -3,26 +3,43 @@ package BinarySearch.OneDimArrays;
 public class PeakElement {
     public static void main(String[] args) {
 //        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
-        int[] arr = {1, 2, 3, 4, 5, 6, 20};
+        int[] arr = {1, 10, 13, 3, 4, 5, 6,2, 1};
         System.out.println(peakElement(arr));
     }
 
     public static int peakElement(int[] arr){
         // Brute force solution
 
-        int n = arr.length;
-        for(int i = 0 ; i < n ; i++){
-
-            boolean left = (i == 0) || (arr[i] >= arr[i-1]);
-            boolean right = (i == n-1) || (arr[i] >= arr[i+1]);
-
-            if(left && right)
-                return i;
-        }
-        return -1;
+//        int n = arr.length;
+//        for(int i = 0 ; i < n ; i++){
+//
+//            boolean left = (i == 0) || (arr[i] >= arr[i-1]);
+//            boolean right = (i == n-1) || (arr[i] >= arr[i+1]);
+//
+//            if(left && right)
+//                return i;
+//        }
+//        return -1;
 
         // Optimal solution
 
+        int n = arr.length;
+        if(n == 1) return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
 
+        int low = 1, high = n-2;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+                return mid;
+            else if(arr[mid] > arr[mid - 1])
+                low = mid + 1;
+            else if(arr[mid] > arr[mid + 1])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
     }
 }
