167e6d7c59d2fa62dff1954d5df7062b3f65a7e6
Pankaj Lakhnavi <pankajlakhnavi2001@gmail.com>
2026-05-16 23:18:03 +0530

    optimal solution

--- Striver/BinarySearch/BS_On_Answers/FindSquareRoot.java
+++ Striver/BinarySearch/BS_On_Answers/FindSquareRoot.java
@@ -3,20 +3,44 @@ package BinarySearch.BS_On_Answers;
 public class FindSquareRoot {
 
     public static void main(String[] args) {
-        System.out.println(squareRoot(32));
+        System.out.println(squareRoot(36));
     }
 
     public static int squareRoot(int n) {
-        int result = 0;
-        for(int i = 1 ; i <= n ; i++){
-//            check if i * i <= n
-            if((long)(i) * i <= n)
-                result = i;
+//        Brute force approach
+
+//        int result = 0;
+//        for(int i = 1 ; i <= n ; i++){
+////            check if i * i <= n
+//            if((long)(i) * i <= n)
+//                result = i;
+//            else{
+//                // break when i * i > n
+//                break;
+//            }
+//        }
+//        return result;
+
+//        Optimal approach using Bs
+
+        if(n < 2)
+            return n;
+
+        int left = 1, right  = n/2, ans = 0;
+        while(left <= right){
+//            find middle point
+            long mid = left + (right - left) / 2;
+
+            if(mid * mid <= n){
+                ans = (int) mid;
+                // move to right half
+                left = (int) mid + 1;
+            }
             else{
-                // break when i * i > n
-                break;
+//                move to left half
+                right = (int) mid - 1;
             }
         }
-        return result;
+        return ans;
     }
 }
